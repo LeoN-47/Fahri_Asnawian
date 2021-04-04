366a410ef9ee84a7ecc29d0fb6f2466568eff6bc
Fahri_Asnawian <fahriasnawian5@gmail.com>
2021-04-04 13:44:31 +0700

    Project Tampilan Login

---
+++ app/src/main/java/com/leon/login/home.java
@@ -0,0 +1,19 @@
+package com.leon.login;
+
+import androidx.appcompat.app.AlertDialog;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.DialogInterface;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
+public class home extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_home);
+    }
+}
